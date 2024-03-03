18afc3efb14bfc79bbf4e77851cf2163af97dfae
Steven Woodward <steveanthony999@gmail.com>
2024-03-02 23:48:57 -0800

    Added exports to empty page components

---
+++ pages/booking.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const booking = () => {
+  return <div>booking</div>;
+};
+
+export default booking;
---
+++ pages/termsofservice.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const termsofservice = () => {
+  return <div>termsofservice</div>;
+};
+
+export default termsofservice;
