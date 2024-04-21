21015d7bb98957a7aa28fa33120eefcd049f5c5c
Steven Woodward <steveanthony999@gmail.com>
2024-04-21 13:07:02 -0700

    Added fetch current user and redirect

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,71 +1,74 @@
-import { UserButton, useAuth } from '@clerk/nextjs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useUser, useAuth } from '@clerk/nextjs';
 
-const DotIcon = () => {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 512 512'
-      fill='currentColor'
-    >
-      <path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z' />
-    </svg>
-  );
-};
-
-const CustomPage = () => {
-  return (
-    <div>
-      <h1>Custom Profile Page</h1>
-      <p>This is the custom profile page</p>
-    </div>
-  );
-};
-
-const Dashboard = () => {
+const GetCurrentUser = () => {
+  const { isSignedIn, user } = useUser();
   const { getToken } = useAuth();
+  const router = useRouter();
+  const [bearerToken, setBearerToken] = useState('');
 
   useEffect(() => {
-    const userToken = async () => {
-      const token = await getToken();
-      console.log(token);
+    // console.log('dashboard redirect component');
+
+    const fetchUserToken = async () => {
+      if (isSignedIn) {
+        const token = await getToken();
+        setBearerToken(token);
+      }
     };
 
-    userToken();
-  }, [getToken]);
+    fetchUserToken();
+
+    if (user === undefined) return; // User state still resolving, do nothing
+
+    if (!isSignedIn) {
+      router.push('/signin');
+      return;
+    }
+
+    if (user && bearerToken) {
+      console.log('calling api: /api/user/getCurrentUser');
+      fetch(`/api/user/getCurrentUser`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${bearerToken}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log('User fetched:', data);
+          if (data.user_role === 'customer_active') {
+            router.push('/customerDashboard');
+          } else if (data.user_role === 'admin_owner') {
+            router.push('/ownerDashboard');
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching user:', error);
+          router.push('/error'); // Redirect to an error page or display a message
+        });
+    }
+  }, [isSignedIn, user, bearerToken, router, getToken]);
 
   return (
-    <div>
-      Dashboard
-      <UserButton afterSignOutUrl='/'>
-        <UserButton.UserProfileLink
-          label='Homepage'
-          url='/'
-          labelIcon={<DotIcon />}
-        />
-        {/* You can pass the content as a component */}
-        <UserButton.UserProfilePage
-          label='Custom Page'
-          url='custom'
-          labelIcon={<DotIcon />}
+    <main className='w-screen h-screen flex flex-col items-center justify-center'>
+      <div role='status'>
+        <svg
+          aria-hidden='true'
+          className='inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-red-600'
+          viewBox='0 0 100 101'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
         >
-          <CustomPage />
-        </UserButton.UserProfilePage>
-
-        {/* You can also pass the content as direct children */}
-        <UserButton.UserProfilePage
-          label='Terms'
-          labelIcon={<DotIcon />}
-          url='termsofservice'
-        >
-          <div>
-            <h1>Custom Terms Page</h1>
-            <p>This is the custom terms page</p>
-          </div>
-        </UserButton.UserProfilePage>
-      </UserButton>
-    </div>
+          {/* SVG content */}
+        </svg>
+        <span className='sr-only'>Loading...</span>
+      </div>
+      <h1 className='mt-8'>Redirecting you to your BoxValet dashboard</h1>
+    </main>
   );
 };
 
-export default Dashboard;
+export default GetCurrentUser;
---
+++ pages/ownerDashboard.jsx
@@ -0,0 +1,73 @@
+import { UserButton, useAuth } from '@clerk/nextjs';
+import { useEffect } from 'react';
+
+// TODO: Fetch the current user and redirect to the appropriate dashboard depending on their user_role
+
+const DotIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 512 512'
+      fill='currentColor'
+    >
+      <path d='M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z' />
+    </svg>
+  );
+};
+
+const CustomPage = () => {
+  return (
+    <div>
+      <h1>Custom Profile Page</h1>
+      <p>This is the custom profile page</p>
+    </div>
+  );
+};
+
+const OwnerDashboard = () => {
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const userToken = async () => {
+      const token = await getToken();
+      console.log(token);
+    };
+
+    userToken();
+  }, [getToken]);
+
+  return (
+    <div>
+      Owner Dashboard
+      <UserButton afterSignOutUrl='/'>
+        <UserButton.UserProfileLink
+          label='Homepage'
+          url='/'
+          labelIcon={<DotIcon />}
+        />
+        {/* You can pass the content as a component */}
+        <UserButton.UserProfilePage
+          label='Custom Page'
+          url='custom'
+          labelIcon={<DotIcon />}
+        >
+          <CustomPage />
+        </UserButton.UserProfilePage>
+
+        {/* You can also pass the content as direct children */}
+        <UserButton.UserProfilePage
+          label='Terms'
+          labelIcon={<DotIcon />}
+          url='termsofservice'
+        >
+          <div>
+            <h1>Custom Terms Page</h1>
+            <p>This is the custom terms page</p>
+          </div>
+        </UserButton.UserProfilePage>
+      </UserButton>
+    </div>
+  );
+};
+
+export default OwnerDashboard;
